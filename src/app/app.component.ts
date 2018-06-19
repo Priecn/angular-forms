6925a10ab22bf4637be0883801256c97512e5687
Priecn <prince.silverarrow@gmail.com>
2018-06-19 08:38:03 +0530

    Template driven approch finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,50 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  @ViewChild('f') userForm: NgForm;
+  defaultSecretValue: string = 'pet';
+  answer: string;
+  genders = ['Male', 'Female'];
+  isSubmitted: boolean = false;
+  user = {
+    name: '',
+    email: '',
+    gender: '',
+    secret: {
+      question: '',
+      answer: ''
+    }
+  }
+  /* onSubmit(form: HTMLFormElement) {
+    console.log(form);
+  } */
+
+  suggestUsername(){
+    this.userForm.form.patchValue({
+      userData : {
+        username: 'suggested name'
+      }
+    });
+
+    /* this.userForm.setValue({
+      userData: {
+        username: 'suggested name',
+        email: ''
+      },
+      secret: '',
+      answer: '',
+      gender: 'Male'
+    }); */
+  }
+
+  onSubmit(form: NgForm) {
+    //console.log(form);
+    this.user.name = form.value.userData.username;
+    this.user.email = form.value.userData.email;
+    this.user.secret.question = form.value.secret;
+    this.user.secret.answer = form.value.answer;
+    this.user.gender = form.value.gender;
+    this.isSubmitted = true;
+    form.reset();
+  }
 }
